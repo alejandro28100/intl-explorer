20282ece7988ea6f3a5bd6e25d3a549248f87285
Alejandro <fuentescletoalejandro@gmail.com>
2022-11-14 21:13:24 -0600

    Integrate MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import createCache from "../utils/createEmotionCache";
+import darkTheme from "../styles/theme/darkTheme";
+
+const clientSideEmotionCache = createCache();
+
+export default function App({
+  pageProps,
+  Component,
+  emotionCache = clientSideEmotionCache,
+}: AppProps & { emotionCache: EmotionCache }) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
