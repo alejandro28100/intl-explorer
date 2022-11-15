63385df0fddea6ec8515e4398ed4808944755b93
Alejandro <fuentescletoalejandro@gmail.com>
2022-11-14 21:03:38 -0600

    Add darkTheme

---
+++ styles/theme/darkTheme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1B1B2F",
+    },
+    secondary: {
+      main: "#1F4068",
+    },
+  },
+});
+
+export default darkTheme;
