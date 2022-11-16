840341cd42d2bb6317432ca18aedefc23f855e00
Alejandro <fuentescletoalejandro@gmail.com>
2022-11-16 01:00:16 -0600

    Feat: Separate fast formatting options from the specific ones
    * Add dayPeriod options

--- pages/constructors/DateTimeFormat.tsx
+++ pages/constructors/DateTimeFormat.tsx
@@ -11,7 +11,17 @@ import {
   FormControlLabel,
   RadioGroup,
   Radio,
+  AccordionSummary,
+  Accordion,
+  AccordionDetails,
+  Alert,
+  AlertTitle,
+  FormGroup,
+  Switch,
+  FormHelperText,
 } from "@mui/material";
+
+import { ExpandMore } from "@mui/icons-material";
 
 import { DATE_STYLES, TIME_STYLES, LOCALES, DAY_PERIODS } from "../../data";
 
@@ -27,11 +37,13 @@ function DateTimeFormat() {
   const [dayPeriod, setDayPeriod] = React.useState<TDayPeriod | undefined>(
     undefined
   );
+  const [hour12, setHour12] = React.useState<boolean | undefined>(undefined);
 
   const formattedDate = new Intl.DateTimeFormat(locale, {
     dateStyle,
     timeStyle,
     dayPeriod,
+    hour12,
   }).format(date);
 
   React.useEffect(() => {
@@ -46,6 +58,7 @@ function DateTimeFormat() {
     ...(dateStyle && { dateStyle }),
     ...(timeStyle && { timeStyle }),
     ...(dayPeriod && { dayPeriod }),
+    ...(hour12 !== undefined && { hour12 }),
   };
 
   const hasOptions = Object.keys(options).length > 0;
@@ -66,8 +79,12 @@ function DateTimeFormat() {
         <Typography component="h1" variant="h6">
           DateTimeFormat
         </Typography>
-        <Stack spacing={2} direction="row" sx={{ flex: 1 }}>
-          <Stack flex="1" spacing={2}>
+        <Stack spacing={2} direction="row">
+          <Stack
+            sx={{ height: "80vh", overflowY: "auto", px: 0.5, pr: 2 }}
+            flex="1"
+            spacing={2}
+          >
             <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
               <InputLabel>Locale</InputLabel>
               <Select
@@ -81,58 +98,105 @@ function DateTimeFormat() {
                 ))}
               </Select>
             </FormControl>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Switch
+                    value={hour12}
+                    onChange={(e) => setHour12(e.target.checked)}
+                  />
+                }
+                label="Hour 12"
+              />
+              <FormHelperText>
+                Whether to use 12-hour time (as opposed to 24-hour time).
+              </FormHelperText>
+            </FormGroup>
+            <Accordion defaultExpanded>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Fast Formatting</Typography>
+              </AccordionSummary>
+              <AccordionDetails
+                sx={{ display: "flex", gap: 2, flexDirection: "column" }}
+              >
+                <FormControl>
+                  <FormLabel>Date style</FormLabel>
+                  <RadioGroup
+                    row
+                    value={dateStyle}
+                    onChange={(e) => setDateStyle(e.target.value as TDateStyle)}
+                  >
+                    {DATE_STYLES.map((value) => (
+                      <FormControlLabel
+                        key={value}
+                        value={value}
+                        control={<Radio />}
+                        label={value}
+                      />
+                    ))}
+                  </RadioGroup>
+                </FormControl>
 
-            <FormControl>
-              <FormLabel>Date style</FormLabel>
-              <RadioGroup
-                row
-                value={dateStyle}
-                onChange={(e) => setDateStyle(e.target.value as TDateStyle)}
+                <FormControl>
+                  <FormLabel>Time style</FormLabel>
+                  <RadioGroup
+                    row
+                    value={timeStyle}
+                    onChange={(e) => setTimeStyle(e.target.value as TTimeStyle)}
+                  >
+                    {TIME_STYLES.map((value) => (
+                      <FormControlLabel
+                        key={value}
+                        value={value}
+                        control={<Radio />}
+                        label={value}
+                      />
+                    ))}
+                  </RadioGroup>
+                </FormControl>
+                <Alert severity="info">
+                  <AlertTitle>Note:</AlertTitle>
+                  <em>dateStyle</em> can be used with <em>timeStyle</em>, but{" "}
+                  <b>not</b> with other options (e.g. <em>weekday</em>,{" "}
+                  <em>hour</em>, <em>month</em> , etc.).
+                </Alert>
+              </AccordionDetails>
+            </Accordion>
+
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
               >
-                {DATE_STYLES.map((value) => (
-                  <FormControlLabel
-                    key={value}
-                    value={value}
-                    control={<Radio />}
-                    label={value}
-                  />
-                ))}
-              </RadioGroup>
-            </FormControl>
-            <FormControl>
-              <FormLabel>Time style</FormLabel>
-              <RadioGroup
-                row
-                value={timeStyle}
-                onChange={(e) => setTimeStyle(e.target.value as TTimeStyle)}
+                <Typography>Specific Formatting</Typography>
+              </AccordionSummary>
+              <AccordionDetails
+                sx={{ display: "flex", gap: 2, flexDirection: "column" }}
               >
-                {TIME_STYLES.map((value) => (
-                  <FormControlLabel
-                    key={value}
-                    value={value}
-                    control={<Radio />}
-                    label={value}
-                  />
-                ))}
-              </RadioGroup>
-            </FormControl>
-            <FormControl>
-              <FormLabel>Day period</FormLabel>
-              <RadioGroup
-                row
-                value={dayPeriod}
-                onChange={(e) => setDayPeriod(e.target.value as TDayPeriod)}
-              >
-                {DAY_PERIODS.map((period) => (
-                  <FormControlLabel
-                    key={period}
-                    value={period}
-                    control={<Radio />}
-                    label={period}
-                  />
-                ))}
-              </RadioGroup>
-            </FormControl>
+                <FormControl>
+                  <FormLabel>Day period</FormLabel>
+                  <RadioGroup
+                    row
+                    value={dayPeriod}
+                    onChange={(e) => setDayPeriod(e.target.value as TDayPeriod)}
+                  >
+                    {DAY_PERIODS.map((period) => (
+                      <FormControlLabel
+                        key={period}
+                        value={period}
+                        control={<Radio />}
+                        label={period}
+                      />
+                    ))}
+                  </RadioGroup>
+                </FormControl>
+              </AccordionDetails>
+            </Accordion>
           </Stack>
           <Stack width="50%" justifyContent="space-between" spacing={2}>
             <Stack alignItems="center">
