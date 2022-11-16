a9eff650156db30a6b9712aa9eab8a9546aeeb17
Alejandro <fuentescletoalejandro@gmail.com>
2022-11-16 01:17:40 -0600

    Add RadioGroup component

---
+++ components/RadioGroup.tsx
@@ -0,0 +1,56 @@
+import {
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  Radio,
+  RadioGroup as MUIRadioGroup,
+  RadioGroupProps,
+} from "@mui/material";
+
+interface IRadioGroupProps {
+  label: string;
+  value: RadioGroupProps["value"];
+  onChange: RadioGroupProps["onChange"];
+  options: (TRadioGroupOptions | string)[];
+}
+
+type TRadioGroupOptions = {
+  label?: string;
+  value: string;
+  id?: string;
+};
+
+function RadioGroup({
+  label,
+  value,
+  onChange,
+  options = [],
+}: IRadioGroupProps) {
+  return (
+    <FormControl>
+      {label && <FormLabel>{label}</FormLabel>}
+      <MUIRadioGroup row value={value} onChange={onChange}>
+        {options.length > 0 &&
+          options.map((option) =>
+            typeof option === "string" ? (
+              <FormControlLabel
+                key={option}
+                value={option}
+                control={<Radio />}
+                label={option}
+              />
+            ) : (
+              <FormControlLabel
+                key={option.id || option.value}
+                value={value}
+                control={<Radio />}
+                label={option.label || option.value}
+              />
+            )
+          )}
+      </MUIRadioGroup>
+    </FormControl>
+  );
+}
+
+export default RadioGroup;
