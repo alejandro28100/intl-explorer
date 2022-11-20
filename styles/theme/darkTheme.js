782938ad5ea867104b851fb5cf4b255925ea3ed3
Alejandro <fuentescletoalejandro@gmail.com>
2022-11-20 02:07:49 -0600

    Add theme colors

--- styles/theme/darkTheme.js
+++ styles/theme/darkTheme.js
@@ -3,14 +3,30 @@ import { createTheme } from "@mui/material/styles";
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
+    text: {
+      primary: "#e3e8ed",
+    },
     primary: {
-      main: "#f67599",
+      main: "#fca6d1",
     },
     secondary: {
       main: "#99d6ea",
     },
     background: {
-      default: "#161821",
+      default: "#212b42",
+      paper: "#1b2334",
+    },
+    info: {
+      main: "#3cc5f8",
+    },
+    warning: {
+      main: "#f3ea5d",
+    },
+    error: {
+      main: "#ff5869",
+    },
+    success: {
+      main: "#7ae1bf",
     },
   },
 });
