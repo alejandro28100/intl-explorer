ee8af8f9a2fd98a12df874e60a1afc3b70ccb643
Alejandro <fuentescletoalejandro@gmail.com>
2022-11-21 18:38:32 -0600

    Feat: Add responsive styles to Navbar
    * For desktop, the navbar shows the logo be and the links on the right
    * For mobile, the navbar has the logo and the links are shown in a toggable drawer component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,40 +1,108 @@
-import { Stack, Link as MUILink } from "@mui/material";
+import {
+  Stack,
+  Link as MUILink,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 import { INTL_METHODS } from "data";
+import { Menu } from "@mui/icons-material";
 import Link from "next/link";
 import Logo from "./Logo";
+import { useState } from "react";
+
+const methodsAvailable = ["DateTimeFormat"];
 
 function Navbar() {
   const links = Object.keys(INTL_METHODS).map((method) => {
-    const href =
-      method === "DateTimeFormat"
-        ? `/constructors/${encodeURIComponent(method)}`
-        : "/comming-soon";
-    return (
-      <MUILink key={method} href={href} component={Link} underline="hover">
-        {method}
-      </MUILink>
-    );
+    const isMethodAvailable = methodsAvailable.includes(method);
+    const href = isMethodAvailable
+      ? `/constructors/${encodeURIComponent(method)}`
+      : "/comming-soon";
+    return {
+      href: href,
+      label: method,
+      isAvailable: isMethodAvailable,
+    };
   });
   return (
     <Stack
       direction="row"
       alignItems="center"
-      py={2}
-      px={4}
       position="sticky"
       top={0}
       width="100%"
       zIndex={10}
       sx={{
+        py: [1, 2],
+        px: [2, 4],
         bgcolor: (theme) => theme.palette.background.paper,
       }}
     >
       <Logo />
-      <Stack direction="row" spacing={2}>
-        {links}
-      </Stack>
+      <Links links={links} />
     </Stack>
   );
 }
+
+interface ILinksProps {
+  links: { href: string; label: string; isAvailable: boolean }[];
+}
+const Links = ({ links }: ILinksProps) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  function toggleMobileMenu() {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  }
+
+  return (
+    <>
+      <Stack
+        sx={{
+          display: ["none", "none", "flex"],
+        }}
+        direction="row"
+        spacing={2}
+      >
+        {links.map((link) => (
+          <MUILink
+            key={link.label}
+            href={link.href}
+            component={Link}
+            underline="hover"
+          >
+            {link.label}
+          </MUILink>
+        ))}
+      </Stack>
+
+      <IconButton
+        sx={{
+          display: ["block", "block", "none"],
+        }}
+        onClick={toggleMobileMenu}
+      >
+        <Menu />
+      </IconButton>
+      <Drawer open={isMobileMenuOpen} anchor="right" onClose={toggleMobileMenu}>
+        <List>
+          {links.map((link) => (
+            <ListItem key={link.label} disablePadding>
+              <ListItemButton href={link.href} LinkComponent={Link}>
+                <ListItemText
+                  primary={link.label}
+                  secondary={link.isAvailable ? "" : "Comming soon"}
+                />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </>
+  );
+};
 
 export default Navbar;
